b6ded74065186e0c5bdda2998ffc510e61fb04b7
inasekin <nasekinid8591@yandex.ru>
2023-08-17 16:20:54 +0300

    add save token

--- weather.js
+++ weather.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
-import {getArgs} from "./helpers/args.js";
-import {printHelp} from "./services/log.service.js";
+import { getArgs } from "./helpers/args.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Токен сохранен');
+    } catch (e) {
+        printError(e.message);
+    }
+};
 
 const initApp = () => {
     const args = getArgs(process.argv);
@@ -15,7 +25,7 @@ const initApp = () => {
     }
 
     if (args.t) {
-
+        return saveToken(args.t);
     }
 }
 
